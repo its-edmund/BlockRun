b3631e3feb5567eae32a2b48c256b23c8f0f1867
Edmund Xin <mxinburritos@gmail.com>
2020-06-17 10:24:49 -0700

    Player Movement Finished
    Camera is bound to player and player can move with "a" and "d" keys

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    public bool moveRight = false;
+    public bool moveLeft = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            moveRight = true;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            moveLeft = true;
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            moveRight = false;
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            moveLeft = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (moveLeft)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
